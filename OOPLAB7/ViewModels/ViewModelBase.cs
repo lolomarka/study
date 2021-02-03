80f92331b963da92c271baa3dc88693f69863918
lolomarka <mark.sheretov@gmail.com>
2021-02-03 11:55:32 +0500

    Lab7InitCommit

---
+++ OOPLAB7/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace OOPLAB7.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
