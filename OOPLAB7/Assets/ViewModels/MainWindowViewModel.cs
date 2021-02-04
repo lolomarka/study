dccf4ee2c5e0ae4f70a3d637d81400b1d79e3c25
lolomarka <mark.sheretov@gmail.com>
2021-02-04 11:07:16 +0500

    Немного улучшил логику. Появились свойства для изменения размера окна.

--- OOPLAB7/Assets/ViewModels/MainWindowViewModel.cs
+++ OOPLAB7/Assets/ViewModels/MainWindowViewModel.cs
@@ -2,19 +2,90 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+
 using ReactiveUI;
 
 namespace Assets.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        private string _dataArray = "Массив";
+        private int _wHeight = 640;
+        private int _wWidth = 320;
+        public int wHeight
+        {
+            get => _wHeight;
+            set => this.RaiseAndSetIfChanged(ref _wHeight, value);
+        }
+        public int wWidth
+        {
+            get => _wHeight;
+            set => this.RaiseAndSetIfChanged(ref _wWidth, value);
+        }
 
+
+
+
+
+        private ArrayType _arrayType;
+
+        /// <summary>
+        /// Данные, записанные в TextBox
+        /// </summary>
+        private string _dataArray = "Массив";
+        /// <summary>
+        /// Свойство для данных, записанных в TextBox(_dataArray)
+        /// </summary>
+        /// <value></value>
         public string DataArray
         {
             get => _dataArray;
             set => this.RaiseAndSetIfChanged(ref _dataArray, value);
         } 
+
+        /// <summary>
+        /// ReadOnly режим textBox-а
+        /// </summary>
+        private bool _isReadOnly = true;
+
+
+        /// <summary>
+        /// Видимость кнопок(по умолчанию false)
+        /// </summary>
+        private bool _isVisibleButtons = false;
+        public bool isVisibleButtons
+        {
+            get => _isVisibleButtons;
+            set => this.RaiseAndSetIfChanged(ref _isVisibleButtons, value);
+        }
+        
+        /// <summary>
+        /// Видимость textbox-а (по умолчанию false)
+        /// </summary>
+        private bool _isVisibleTextBox = false;
+        public bool isVisibleTextBox
+        {
+            get => _isVisibleTextBox;
+            set => this.RaiseAndSetIfChanged(ref _isVisibleTextBox, value);
+        }
+
+        /// <summary>
+        /// Свойство для переключателя _isReadOnly
+        /// </summary>
+        /// <value></value>
+        public bool isReadOnly
+        {
+            get => _isReadOnly;
+            set => this.RaiseAndSetIfChanged(ref _isReadOnly, value);
+        }
+
+
+        private bool _isVisibleGroup1 = false;
+        public bool isVisibleGroup1
+        {
+            get => _isVisibleGroup1;
+            set => this.RaiseAndSetIfChanged(ref _isVisibleGroup1, value);
+        }
+        
 
 
         public string Greeting => "Welcome to Avalonia!";
@@ -26,20 +97,51 @@ namespace Assets.ViewModels
 
         public void ResetCommand()
         {
-
+            GC.Collect();
+            DataArray = "Массив";
+            isVisibleButtons = false;
+            isReadOnly = true;
+            isVisibleTextBox = false;
+            
         }
 
         public void ArrayInitCommand()
         {
-
+            isVisibleButtons = true;
+            _arrayType = ArrayType.OneDim;
         }
         public void MatrixInitCommand()
         {
-            
+            isVisibleButtons = true;
+            _arrayType = ArrayType.DoubleDim;
         }
         public void JaggedArrayInitCommand()
         {
-            
+            isVisibleButtons = true;
+            _arrayType = ArrayType.Jagged;
+        }
+
+
+        public void ManualInputCommand()
+        {
+            switch(_arrayType)
+            {
+                case ArrayType.OneDim:
+                {
+                    isVisibleGroup1 = true;
+                    break;
+                }
+                case ArrayType.DoubleDim:
+                {
+
+                    break;
+                }
+                case ArrayType.Jagged:
+                {
+
+                    break;
+                }
+            }
         }
     }
 }
